99373dd0a500919149ccb5d4186d13585c71f1dd
Solomon Kibon <solomonkibon@gmail.com>
2023-08-10 15:40:07 +0300

    update

--- IRIS_App.py
+++ IRIS_App.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+
 st.subheader('By Solomon Kibon')
 # Load the trained model
 model = joblib.load("best_model.pkl")
@@ -13,10 +14,10 @@ st.title('Iris Flower Classification')
 st.write('Enter the values of iris features to predict the flower species.')
 
 # Define input fields for features
-sepal_length = st.number_input("sepal_length", min_value=4.0, max_value=8.0, value=5.8, step=0.1)
-sepal_width = st.number_input("sepal_width", min_value=2.0, max_value=4.5, value=3.0, step=0.1)
-petal_length = st.number_input("petal_length", min_value=1.0, max_value=7.0, value=4.35, step=0.1)
-petal_width = st.number_input("petal_width", min_value=0.1, max_value=2.5, value=1.3, step=0.1)
+sepal_length = st.number_input("sepal_length", min_value=4.0, max_value=8.0, value=5.1, step=0.1)
+sepal_width = st.number_input("sepal_width", min_value=2.0, max_value=4.5, value=3.5, step=0.1)
+petal_length = st.number_input("petal_length", min_value=1.0, max_value=7.0, value=1.4, step=0.1)
+petal_width = st.number_input("petal_width", min_value=0.1, max_value=2.5, value=0.2, step=0.1)
 # Create a button for making predictions
 if st.button("Predict"):
     # Process input values
@@ -37,8 +38,8 @@ input_data_scaled = scaler.fit_transform(input_data)
 prediction = model.predict(input_data_scaled)
 #display prediction
 if prediction==0:
-    st.write(f'Predicted Species: Iris-setosa')
+    st.success(f'Predicted Species: Iris-setosa')
 elif prediction==1:
-    st.write(f'Predicted Species: Iris-versicolor')
+    st.success(f'Predicted Species: Iris-versicolor')
 else:
-    st.write(f'Predicted Species: Iris-virginica')
+    st.wrsuccess(f'Predicted Species: Iris-virginica')
